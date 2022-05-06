f9d842c66cc90275d77ec8e78a7207beab6fd737
KNSama <minhhieusss111@gmail.com>
2022-05-07 01:43:49 +0700

    viet hoa

---
+++ resources/views/backend/category/index.blade.php
@@ -0,0 +1,156 @@
+@extends('backend.layouts.master')
+
+@section('main-content')
+ <!-- DataTales Example -->
+ <div class="card shadow mb-4">
+     <div class="row">
+         <div class="col-md-12">
+            @include('backend.layouts.notification')
+         </div>
+     </div>
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary float-left">Danh sách danh mục</h6>
+      <a href="{{route('Category.create')}}" class="btn btn-primary btn-sm float-right" data-toggle="tooltip" data-placement="bottom" title="Add User"><i class="fas fa-plus"></i> Thêm danh mục</a>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        @if(count($categories)>0)
+        <table class="table table-bordered" id="banner-dataTable" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th>STT</th>
+              <th>Tiêu đề</th>
+              <th>Slug</th>
+              <th>Là Parent</th>
+              <th>Danh mục Parent</th>
+              <th>Photo</th>
+              <th>Trạng thái</th>
+              <th>Hành động</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <th>STT</th>
+              <th>Tiêu đề</th>
+              <th>Slug</th>
+              <th>Là Parent</th>
+              <th>Danh mục Parent</th>
+              <th>Photo</th>
+              <th>Trạng thái</th>
+              <th>Hành động</th>
+            </tr>
+          </tfoot>
+          <tbody>
+
+            @foreach($categories as $Category)
+              @php
+              @endphp
+                <tr>
+                    <td>{{$Category->id}}</td>
+                    <td>{{$Category->title}}</td>
+                    <td>{{$Category->slug}}</td>
+                    <td>{{(($Category->is_parent==1)? 'Có': 'Không')}}</td>
+                    <td>
+                        {{$Category->parent_info->title ?? ''}}
+                    </td>
+                    <td>
+                        @if($Category->photo)
+                            <img src="{{$Category->photo}}" class="img-fluid" style="max-width:80px" alt="{{$Category->photo}}">
+                        @else
+                            <img src="{{asset('backend/img/thumbnail-default.jpg')}}" class="img-fluid" style="max-width:80px" alt="avatar.png">
+                        @endif
+                    </td>
+                    <td>
+                        @if($Category->status=='active')
+                            <span class="badge badge-success">{{$Category->status}}</span>
+                        @else
+                            <span class="badge badge-warning">{{$Category->status}}</span>
+                        @endif
+                    </td>
+                    <td>
+                        <a href="{{route('Category.edit',$Category->id)}}" class="btn btn-primary btn-sm float-left mr-1" style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" title="edit" data-placement="bottom"><i class="fas fa-edit"></i></a>
+                    <form method="POST" action="{{route('Category.destroy',[$Category->id])}}">
+                      @csrf
+                      @method('delete')
+                          <button class="btn btn-danger btn-sm dltBtn" data-id={{$Category->id}} style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" data-placement="bottom" title="Xoá"><i class="fas fa-trash-alt"></i></button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+          </tbody>
+        </table>
+        <span style="float:right">{{$categories->links()}}</span>
+        @else
+          <h6 class="text-center">Không tìm thấy danh mục!!! Hãy tạo thêm danh mục</h6>
+        @endif
+      </div>
+    </div>
+</div>
+@endsection
+
+@push('styles')
+  <link href="{{asset('backend/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css" />
+  <style>
+      div.dataTables_wrapper div.dataTables_paginate{
+          display: none;
+      }
+  </style>
+@endpush
+
+@push('scripts')
+
+  <!-- Page level plugins -->
+  <script src="{{asset('backend/vendor/datatables/jquery.dataTables.min.js')}}"></script>
+  <script src="{{asset('backend/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="{{asset('backend/js/demo/datatables-demo.js')}}"></script>
+  <script>
+
+      $('#banner-dataTable').DataTable( {
+            "columnDefs":[
+                {
+                    "orderable":false,
+                    "targets":[3,4,5]
+                }
+            ]
+        } );
+
+        // Sweet alert
+
+        function deleteData(id){
+
+        }
+  </script>
+  <script>
+      $(document).ready(function(){
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+          $('.dltBtn').click(function(e){
+            var form=$(this).closest('form');
+              var dataID=$(this).data('id');
+              // alert(dataID);
+              e.preventDefault();
+              swal({
+                    title: "Bạn có chắc không?",
+                    text: "Một khi xoá, bạn sẽ không thể hồi phục lại dữ liệu này!",
+                    icon: "Cảnh báo",
+                    buttons: true,
+                    dangerMode: true,
+                })
+                .then((willDelete) => {
+                    if (willDelete) {
+                       form.submit();
+                    } else {
+                        swal("Dữ liệu của bạn đã an toàn!");
+                    }
+                });
+          })
+      })
+  </script>
+@endpush
---
+++ resources/views/user/review/show.blade.php
@@ -0,0 +1,152 @@
+@extends('user.layouts.master')
+
+@section('title','Order Detail')
+
+@section('main-content')
+<div class="card">
+<h5 class="card-header">Order       <a href="{{route('order.pdf',$order->id)}}" class=" btn btn-sm btn-primary shadow-sm float-right"><i class="fas fa-download fa-sm text-white-50"></i> Generate PDF</a>
+  </h5>
+  <div class="card-body">
+    @if($order)
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+            <th>STT</th>
+            <th>ID đơn hàng</th>
+            <th>Tên</th>
+            <th>Email</th>
+            <th>Số lượng</th>
+            <th>Charge</th>
+            <th>Tổng cộng</th>
+            <th>Trạng thái</th>
+            <th>Hành động</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+            <td>{{$order->id}}</td>
+            <td>{{$order->cart_id}}</td>
+            <td>{{$order->first_name}} {{$order->last_name}}</td>
+            <td>{{$order->email}}</td>
+            <td>{{$order->quantity}}</td>
+            <td>${{number_format($order->delivery_charge,2)}}</td>
+            <td>${{number_format($order->total_amount,2)}}</td>
+            <td>
+                @if($order->status=='new')
+                  <span class="badge badge-primary">{{$order->status}}</span>
+                @elseif($order->status=='process')
+                  <span class="badge badge-warning">{{$order->status}}</span>
+                @elseif($order->status=='delivered')
+                  <span class="badge badge-success">{{$order->status}}</span>
+                @else
+                  <span class="badge badge-danger">{{$order->status}}</span>
+                @endif
+            </td>
+            <td>
+                <a href="{{route('order.edit',$order->id)}}" class="btn btn-primary btn-sm float-left mr-1" style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" title="edit" data-placement="bottom"><i class="fas fa-edit"></i></a>
+                <form method="POST" action="{{route('order.destroy',[$order->id])}}">
+                  @csrf 
+                  @method('delete')
+                      <button class="btn btn-danger btn-sm dltBtn" data-id={{$order->id}} style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" data-placement="bottom" title="Xoá"><i class="fas fa-trash-alt"></i></button>
+                </form>
+            </td>
+          
+        </tr>
+      </tbody>
+    </table>
+
+    <section class="confirmation_part section_padding">
+      <div class="order_boxes">
+        <div class="row">
+          <div class="col-lg-6 col-lx-4">
+            <div class="order-info">
+              <h4 class="text-center pb-4">THÔN TIN ĐƠN HÀNG</h4>
+              <table class="table">
+                    <tr class="">
+                        <td>ID đơn hàng</td>
+                        <td> : {{$order->cart_id}}</td>
+                    </tr>
+                    <tr>
+                        <td>Ngày đặt</td>
+                        <td> : {{$order->created_at->diffForHumans()}}</td>
+                    </tr>
+                    <tr>
+                        <td>Số lượng</td>
+                        <td> : {{$order->quantity}}</td>
+                    </tr>
+                    <tr>
+                        <td>Tình trạng đơn hàng</td>
+                        <td> : {{$order->status}}</td>
+                    </tr>
+                    <tr>
+                        <td>Phí Shipping</td>
+                        <td> : $ {{number_format($order->delivery_charge,2)}}</td>
+                    </tr>
+                    <tr>
+                        <td>Tổng cộng</td>
+                        <td> : $ {{number_format($order->total_amount,2)}}</td>
+                    </tr>
+                    <tr>
+                        <td>Phương thức thanh toán</td>
+                        <td> : </td>
+                    </tr>
+                    <tr>
+                        <td>Trạng thái thanh toán</td>
+                        <td> : </td>
+                    </tr>
+              </table>
+            </div>
+          </div>
+
+          <div class="col-lg-6 col-lx-4">
+            <div class="shipping-info">
+              <h4 class="text-center pb-4">THÔNG TIN SHIPPING</h4>
+              <table class="table">
+                    <tr class="">
+                        <td>Tên đầyy đủ</td>
+                        <td> : {{$order->first_name}} {{$order->last_name}}</td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td> : {{$order->email}}</td>
+                    </tr>
+                    <tr>
+                        <td>Điện thoại</td>
+                        <td> : {{$order->phone}}</td>
+                    </tr>
+                    <tr>
+                        <td>Địa chỉ</td>
+                        <td> : {{$order->address1}}, {{$order->address2}}</td>
+                    </tr>
+                    <tr>
+                        <td>Quốc gia</td>
+                        <td> : {{$order->country}}</td>
+                    </tr>
+                    <tr>
+                        <td>Post Code</td>
+                        <td> : {{$order->post_code}}</td>
+                    </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    @endif
+
+  </div>
+</div>
+@endsection
+
+@push('styles')
+<style>
+    .order-info,.shipping-info{
+        background:#ECECEC;
+        padding:20px;
+    }
+    .order-info h4,.shipping-info h4{
+        text-decoration: underline;
+    }
+
+</style>
+@endpush
